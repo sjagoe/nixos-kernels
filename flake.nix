2e71b1e3fe2f955e432fd4efcc4e81aca335a478
Simon Jagoe <simon@simonjagoe.com>
2026-05-14 21:35:11 +0300

    Flake of latest kernel versions

--- flake.nix
+++ flake.nix
@@ -7,8 +7,50 @@
 
   outputs = { self, nixpkgs }:
     let
-      kernelHashes = import ./kernels.json;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      kernelVersions =
+        let
+          kernelHashes = builtins.fromJSON (builtins.readFile ./kernels.json);
+        in
+          builtins.listToAttrs
+            (map (system: { name = system; value = kernelHashes; }) systems);
+
+      overrideKernel = pkgs: version: hash:
+        let
+          inherit (pkgs) lib;
+
+          major = lib.versions.major version;
+          minor = lib.versions.minor version;
+
+          kernelFor = version:
+            pkgs."linux_${major}_${minor}";
+        in
+          (kernelFor version).override {
+            argsOverride = {
+              src = pkgs.fetchurl {
+                url = "mirror://kernel/linux/kernel/v${major}.x/linux-${version}.tar.xz";
+                inherit hash;
+              };
+              inherit version;
+              modDirVersion = version;
+            };
+          };
+
+      generatePackages = system: hashes:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          versions = builtins.attrNames hashes;
+          nameKernel = version: "linux_${builtins.replaceStrings ["."] ["_"] version}";
+          hash = version: hashes."${version}";
+        in
+          builtins.listToAttrs
+            (builtins.map
+              (version: { name = nameKernel version; value = overrideKernel pkgs version (hash version); })
+              versions);
     in
       {
+        packages = builtins.mapAttrs
+          (system: versions: generatePackages system versions)
+          kernelVersions;
       };
 }
