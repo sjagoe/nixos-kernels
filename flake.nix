e9d939ea16601be99a3544148127d35f383dec89
Simon Jagoe <simon@simonjagoe.com>
2026-08-02 22:56:49 +0300

    Fix listing all versions and add the latest version explicitly

--- flake.nix
+++ flake.nix
@@ -2,15 +2,17 @@
   description = "NixOS Kernels from mainline kernel.org";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
+      kernels-json = builtins.fromJSON (builtins.readFile ./kernels.json);
+      availableReleases = kernels-json.versions;
       kernelVersions =
         let
-          kernelHashes = builtins.fromJSON (builtins.readFile ./kernels.json);
+          kernelHashes = kernels-json.hashes;
         in
           builtins.listToAttrs
             (map (system: { name = system; value = kernelHashes; }) systems);
@@ -39,14 +41,41 @@
       generatePackages = system: hashes:
         let
           pkgs = import nixpkgs { inherit system; };
+          inherit (pkgs) lib;
+
+          versionCmp = a: b: (builtins.compareVersions a b) > 0;
+          selectVersions = kver:
+            let
+              re = "^${lib.strings.escapeRegex kver}(\..*|$)";
+            in
+              builtins.sort versionCmp
+                (builtins.filter (i: (builtins.match re i) != null)
+                  (builtins.attrNames hashes));
+
+          latestVersionFor = kver:
+            builtins.elemAt (selectVersions kver) 0;
+
           versions = builtins.attrNames hashes;
-          nameKernel = version: "linux_${builtins.replaceStrings ["."] ["_"] (pkgs.lib.versions.majorMinor version)}";
+          nameKernel = version: "linux_${builtins.replaceStrings ["."] ["_"] version}";
           hash = version: hashes."${version}";
-        in
-          builtins.listToAttrs
+          kernelsByVersion = builtins.listToAttrs
             (builtins.map
-              (version: { name = nameKernel version; value = overrideKernel pkgs version (hash version); })
+              (version: { name = version; value = overrideKernel pkgs version (hash version); })
               versions);
+
+          latestKernelFor = release:
+            let
+              version = latestVersionFor release;
+            in
+              { name = nameKernel "${release}_latest"; value = kernelsByVersion.${version}; };
+
+          latest = builtins.map
+            (release: latestKernelFor release)
+            availableReleases;
+        in
+          (lib.mapAttrs'
+            (version: package: { name = nameKernel version; value = package; })
+            kernelsByVersion) // (builtins.listToAttrs latest);
     in
       {
         packages = builtins.mapAttrs
