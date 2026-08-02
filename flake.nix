535d288d9d9a6cba6397e8ba76eec47a4ccbd596
Simon Jagoe <simon@simonjagoe.com>
2026-08-03 02:57:03 +0300

    Allow nvidia-settings

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
                 config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
                   "nvidia-x11"
                   "nvidia-kernel-modules"
+                  "nvidia-settings"
                 ];
               };
             in
