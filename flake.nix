e4f9e57eff8d5cac8744a44ae3e2547381115686
Simon Jagoe <simon@simonjagoe.com>
2026-08-07 16:23:54 +0300

    Track nixos-26.05-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Kernels from mainline kernel.org";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05-small";
   };
 
   outputs = { self, nixpkgs }:
