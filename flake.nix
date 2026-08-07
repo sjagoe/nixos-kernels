1ce47233897985092cb9fe35f1eb5dcb13c242c5
Simon Jagoe <simon@simonjagoe.com>
2026-08-07 10:08:26 +0300

    Skip unnecessary CI jobs (#18)
    * Add flake CI output for determining cache status of paths
    
    * Skip unnecessary builds when outputs are cached by nixos
    
    * Log store path
    
    * Force substituters for build
    
    * Only clean disk space if we need to do a build
    
    (cherry picked from commit 39eb4a325a7711ac3956a1d6912c10115610a802)
    
    * Fix skipping builds
    
    (cherry picked from commit 67d586b2a203b29d8f23c293d9d5f12cf4589fe2)

--- flake.nix
+++ flake.nix
@@ -52,9 +52,26 @@
       {
         inherit packages;
         ci.build =
+          let
+            pathToHash = path:
+            builtins.elemAt
+              (builtins.match "^/nix/store/([^-]+).*" path) 0;
+            outputPaths = package: map (out: package.${out}.outPath) package.outputs;
+            outputHashes = paths: map pathToHash paths;
+          in
           lib.flatten (map
             ({ runs-on, arch, pkgs }:
-              map (pkg: { inherit runs-on nixos-release; build = ".#packages.${arch}.${pkg}"; }) (builtins.attrNames pkgs))
+              map (pkg:
+                let
+                  paths = outputPaths pkgs.${pkg};
+                in
+                  {
+                    inherit runs-on nixos-release;
+                    build = ".#packages.${arch}.${pkg}";
+                    outputHashes = (outputHashes paths);
+                    paths = paths;
+                  })
+                (builtins.attrNames pkgs))
             (lib.mapAttrsToList
               (arch: pkgs: { runs-on = runner-label arch; inherit arch pkgs; })
               packages));
